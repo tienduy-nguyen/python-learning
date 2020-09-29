7bac91793d607e33a94f0a7f66b2ee4ab7439f08
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-29 23:55:18 +0200

    Fix error not found camelcase module

--- basics/modules.py
+++ basics/modules.py
@@ -1,3 +1,4 @@
+
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
 
 # Core modules
@@ -6,10 +7,11 @@ from datetime import date
 import time
 from time import time
 
-# Pip modules
-import camelcase
+# Pip module
+# import camelcase
+# from camelcase import CamelCase
 
-# Custom modules
+# Import custom module
 import validator
 from validator import validate_email
 
@@ -17,11 +19,11 @@ from validator import validate_email
 today = date.today()
 timestamp = time()
 
-camel = camelcase.CamelCase()
-text = 'hello there world'
+# c = CamelCase()
+# print(c.hump('hello there world'))
 
-email = 'test@test.com'
+email = 'test#test.com'
 if validate_email(email):
   print('Email is valid')
 else:
-  print('Not an email')
+  print('Email is bad')
